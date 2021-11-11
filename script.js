9be4e9b0a2c5877fb395b80deebbb767b4e3e5d6
Royal <royalhuseyn902@gmail.com>
2021-11-11 22:44:24 +0900

    Created the 16x16 grid

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const cdiv = document.querySelector('.container');
 // Add 16 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
-  div.style.cssText = "border: 1px solid black; height: 20px; width: 20px";
+  div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
   div.style.display = "inline-block";
   div.style.boxSizing = "border-box";
-  div.style.verticalAlign = "bottom";
-  div.setAttribute('id', `div${i}`);
   cdiv.appendChild(div);
 }
