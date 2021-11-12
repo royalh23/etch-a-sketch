05b881a2aef3e43ad8f919222296500abf1c841c
Royal <royalhuseyn902@gmail.com>
2021-11-12 18:09:15 +0900

    Will start over

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const cdiv = document.querySelector('.container');
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
   div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-  div.style.display = "inline-block";
   div.style.boxSizing = "border-box";
   cdiv.appendChild(div);
 }
