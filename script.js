4a33b621ee14547abf88f9576b44d8a6bde32164
Royal <royalhuseyn902@gmail.com>
2021-11-12 22:10:02 +0900

    Nothing yet

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const cdiv = document.querySelector('.container');
 // Add 16 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
-  div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-  div.style.boxSizing = "border-box";
+  div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";  
   cdiv.appendChild(div);
 }
