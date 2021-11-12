3ee931704f37fa5893549ad2441b53bdedd5eb43
Royal <royalhuseyn902@gmail.com>
2021-11-12 23:44:18 +0900

    Created the grid

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const cdiv = document.querySelector('.container');
 
-// Add 16 divs
+// Add 256 divs
 for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
-  div.style.cssText = "border: 1px solid black; height: 25px; width: 25px";  
   cdiv.appendChild(div);
 }
