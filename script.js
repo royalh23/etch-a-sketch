9012ae1413d41d247c9fcbe027eefe90dfe741d8
Royal <royalhuseyn902@gmail.com>
2021-11-14 09:37:58 +0900

    Added the hover effect to grid and the button

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ for (let i = 1; i < 257; i++) {
   const div = document.createElement('div');
   cdiv.appendChild(div);
 }
+
+// Add event listener to square divs
+cdiv.childNodes.forEach(div => div.addEventListener('mouseenter', changeColor));
+
+function changeColor(e) {
+  e.target.style.background = "black";
+}
