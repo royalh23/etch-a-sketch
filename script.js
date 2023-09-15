e18415360a5133349d4495d98e936fb787cb7c43
Royal <royalhuseyn902@gmail.com>
2023-09-15 21:32:02 +0400

    Remove class from divs, style them totally via JS

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const container = document.querySelector(".container");
+function changeBg(e) {
+  console.log(e.target);
+}
+
 
 function createGrid() {
   for (let i = 1; i <= 256; i++) {
+    const container = document.querySelector(".container");
     const square = document.createElement("div");
-    square.classList.add("square");
+    square.style.border = "1px solid black";
     square.style.width = `${Math.floor(500/16)}px`;
     square.style.height = `${Math.floor(500/16)}px`;
     container.appendChild(square);
