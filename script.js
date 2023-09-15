b20dcf71d0183dd52c3f76c6390fc57c19a4595c
Royal <royalhuseyn902@gmail.com>
2023-09-15 21:01:46 +0400

    Set dimensions of squares via JS, instead of CSS So that divs' dimensions can easily be adjusted when their amount increases or decreases

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function createGrid() {
   for (let i = 1; i <= 256; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = `${Math.floor(500/16)}px`;
+    square.style.height = `${Math.floor(500/16)}px`;
     container.appendChild(square);
   }
 }
