9fc47549599c75af37c1fb8579c0f3e7534f9f7d
Royal <royalhuseyn902@gmail.com>
2023-09-15 20:45:13 +0400

    Create a function for creating a grid and style squares

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector(".container");
 
 function createGrid() {
-  
+  for (let i = 1; i <= 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
 }
+
+createGrid();
