3a719b6c3a432657fdd951254c3baf8df543f1cf
Royal <royalhuseyn902@gmail.com>
2023-09-18 20:50:28 +0400

    Add an event listener to the button So that new grids can be created with the given number

--- script.js
+++ script.js
@@ -2,19 +2,27 @@ function changeBg(e) {
   e.target.style.backgroundColor = "black";
 }
 
-function createGrid() {
-  for (let i = 1; i <= 256; i++) {
+function createGrid(n) {
+  for (let i = 1; i <= n * n; i++) {
     const square = document.createElement("div");
     square.style.border = "1px solid black";
-    square.style.width = `${Math.floor(500/16)}px`;
-    square.style.height = `${Math.floor(500/16)}px`;
+    square.style.width = `${500/n}px`;
+    square.style.height = `${500/n}px`;
     container.appendChild(square);
   }
 }
 
 
 const container = document.querySelector(".container");
-createGrid();
-const squares = document.querySelectorAll(".container>div");
+const button = document.querySelector("button");
 
+createGrid(16);
+
+const squares = document.querySelectorAll(".container>div");
 squares.forEach(square => square.addEventListener("mouseover", changeBg));
+
+button.addEventListener("click", () => {
+  let number = +prompt("Enter the number of squares per side"); 
+  container.innerHTML = "";
+  createGrid(number);
+});
