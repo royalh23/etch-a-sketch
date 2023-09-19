c9b9703833adbfc6ea4e2789629c0ed53c016661
Royal <royalhuseyn902@gmail.com>
2023-09-19 12:45:25 +0400

    Change divs' backgrounds to random color

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function changeBg(e) {
-  e.target.style.backgroundColor = "black";
+  const hexChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.floor(Math.random() * hexChars.length);
+    hexColor += hexChars[randomNumber];
+  }
+  e.target.style.backgroundColor = `${hexColor}`;
 }
 
 function createGrid(n) {
